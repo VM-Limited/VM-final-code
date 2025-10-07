218ff22b87b2e4448187c38da4dfd97e75630262
unknown <mansoor.qureshi05@gmail.com>
2025-10-07 12:52:34 +0100

    bastion

---
+++ Child/vnet/Bastionhost/variable.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {}
+variable "subnet_name" {}
+variable "vnet_name" {}     
+variable "pip_name" {}
+variable "subnet_id" {}
+variable "public_ip_address_id" {}  
+variable "location" {}
+variable "resource_group_name" {}
+variable "name" {}
+variable "rg_location" {}
+
